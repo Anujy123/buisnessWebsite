d5d2494494b306102b3d176a33954726d97d5023
Anuj <anuj852787@gmail.com>
2022-04-15 20:05:48 +0530

    commit

---
+++ script.js
@@ -0,0 +1,174 @@
+// alert('hello world')
+
+// this is to make show and hide button in container 2 
+
+
+let  paragraph = document.getElementById("para")
+
+if (paragraph.style.display!="none") {
+    paragraph.style.display="none"
+}
+
+let butt = document.getElementById("btn")
+
+function show () {
+    if (paragraph.style.display=="none") {
+        paragraph.style.display = "block"
+    }
+     
+}
+
+function hide () {
+    if (paragraph.style.display=="block") {
+        paragraph.style.display = "none"
+    }
+     
+}
+
+// this is the container 3's javasctipt 
+// this is the first card 
+
+
+let paragraph1 = document.getElementById("para1")
+
+if (paragraph1.style.display!="none") {
+    paragraph1.style.display="none"
+
+}
+
+let butt1 = document.getElementById("btn1")
+
+function show1() {
+    if (paragraph1.style.display=="none") {
+        paragraph1.style.display="block"
+    } 
+
+}
+
+function hide1() {
+    if (paragraph1.style.display=="block") {
+        paragraph1.style.display="none"
+    } 
+
+}
+
+
+// this is the sencond card 
+
+
+let paragraph2 = document.getElementById("para2")
+
+if (paragraph2.style.display!="none") {
+    paragraph2.style.display="none"
+
+}
+
+let butt2 = document.getElementById("btn2")
+
+function show2() {
+    if (paragraph2.style.display=="none") {
+        paragraph2.style.display="block"
+    } 
+
+}
+
+function hide2() {
+    if (paragraph2.style.display=="block") {
+        paragraph2.style.display="none"
+    } 
+
+}
+
+// this is the third card 
+let paragraph3 = document.getElementById("para3")
+
+if (paragraph3.style.display!="none") {
+    paragraph3.style.display="none"
+
+}
+
+let butt3 = document.getElementById("btn3")
+
+function show3() {
+    if (paragraph3.style.display=="none") {
+        paragraph3.style.display="block"
+    } 
+
+}
+
+function hide3() {
+    if (paragraph3.style.display=="block") {
+        paragraph3.style.display="none"
+    } 
+
+}
+
+// this is the fourt card 
+let paragraph4 = document.getElementById("para4")
+
+if (paragraph4.style.display!="none") {
+    paragraph4.style.display="none"
+
+}
+
+let butt4 = document.getElementById("btn4")
+
+function show4() {
+    if (paragraph4.style.display=="none") {
+        paragraph4.style.display="block"
+    } 
+
+}
+
+function hide4() {
+    if (paragraph4.style.display=="block") {
+        paragraph4.style.display="none"
+    } 
+
+}
+// this is the fifth card 
+let paragraph5 = document.getElementById("para5")
+
+if (paragraph5.style.display!="none") {
+    paragraph5.style.display="none"
+
+}
+
+let butt5 = document.getElementById("btn5")
+
+function show5() {
+    if (paragraph5.style.display=="none") {
+        paragraph5.style.display="block"
+    } 
+
+}
+
+function hide5() {
+    if (paragraph5.style.display=="block") {
+        paragraph5.style.display="none"
+    } 
+
+}
+// this is the sixth card 
+let paragraph6 = document.getElementById("para6")
+
+if (paragraph6.style.display!="none") {
+    paragraph6.style.display="none"
+
+}
+
+let butt6 = document.getElementById("btn6")
+
+function show6() {
+    if (paragraph6.style.display=="none") {
+        paragraph6.style.display="block"
+    } 
+
+}
+
+function hide6() {
+    if (paragraph6.style.display=="block") {
+        paragraph6.style.display="none"
+    } 
+
+}
