85b2b3145515101ba4e4af423fc4e4a3e076ad24
Anuj <anuj852787@gmail.com>
2022-04-18 19:42:54 +0530

    fourth commit

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // alert('hello world')
 
+// this is to add a welcome massage to the viewers 
+var music = new Audio('1.mp3')
+
+// if (music.paused) {
+//     music.play()
+// }
+
 // this is to make show and hide button in container 2 
-
-
 let paragraph = document.getElementById("para")
 
 if (paragraph.style.display != "none") {
@@ -14,9 +19,11 @@ let butt = document.getElementById("btn")
 function show() {
     if (paragraph.style.display == "none") {
         paragraph.style.display = "block"
+        music.play()
     }
     else {
         paragraph.style.display = "none"
+        music.play()
     }
 
 }
@@ -39,9 +46,11 @@ let butt1 = document.getElementById("btn1")
 function show1() {
     if (paragraph1.style.display == "none") {
         paragraph1.style.display = "block"
+        music.play()
     }
     else {
         paragraph1.style.display = "none"
+        music.play()
     }
 }
 
@@ -63,9 +72,11 @@ let butt2 = document.getElementById("btn2")
 function show2() {
     if (paragraph2.style.display == "none") {
         paragraph2.style.display = "block"
+        music.play()
     }
     else {
         paragraph2.style.display = "none"
+        music.play()
     }
 
 }
@@ -85,9 +96,11 @@ let butt3 = document.getElementById("btn3")
 function show3() {
     if (paragraph3.style.display == "none") {
         paragraph3.style.display = "block"
+        music.play()
     }
     else {
         paragraph3.style.display = "none"
+        music.play()
     }
 }
 
@@ -106,9 +119,11 @@ let butt4 = document.getElementById("btn4")
 function show4() {
     if (paragraph4.style.display == "none") {
         paragraph4.style.display = "block"
+        music.play()
     }
     else {
         paragraph4.style.display = "none"
+        music.play()
 
     }
 }
@@ -127,9 +142,11 @@ let butt5 = document.getElementById("btn5")
 function show5() {
     if (paragraph5.style.display == "none") {
         paragraph5.style.display = "block"
+        music.play()
     }
     else{
         paragraph5.style.display = "none"
+        music.play()
 
     }
 }
@@ -148,9 +165,11 @@ let butt6 = document.getElementById("btn6")
 function show6() {
     if (paragraph6.style.display == "none") {
         paragraph6.style.display = "block"
+        music.play()
     }
     else{
         paragraph6.style.display = "none"
+        music.play()
 
     }
 }
